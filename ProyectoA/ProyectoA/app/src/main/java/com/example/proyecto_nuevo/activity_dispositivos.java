c224cfa123a3c803cc54ffbc5cc26486add75ca3
NiAlej <131830890+NiAlej@users.noreply.github.com>
2023-10-26 00:37:07 -0300

    Version2 25/10
    asyntask y thread

--- ProyectoA/ProyectoA/app/src/main/java/com/example/proyecto_nuevo/activity_dispositivos.java
+++ ProyectoA/ProyectoA/app/src/main/java/com/example/proyecto_nuevo/activity_dispositivos.java
@@ -5,16 +5,17 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
-public class activity_principal extends AppCompatActivity {
+public class activity_dispositivos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_principal);
+        setContentView(R.layout.activity_dispositivos);
     }
-
-    public void VerCategorias(View view){
-        Intent intent = new Intent(this, activity_catalogo.class);
+    public void VerReview(View view){
+        Intent intent = new Intent(this, activity_review.class);
         startActivity(intent);
     }
+
+
 }
